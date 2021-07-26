c87138744dbe533c99eb682bffc068f2caf15953
max <max.putilov@gmail.com>
2021-07-26 13:59:21 +0300

    splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,18 @@
+/// <reference types="@capacitor/splash-screen" />
+
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'vow-docs',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+    },
+  },
 };
 
 export default config;
